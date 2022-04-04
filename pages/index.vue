dabb4a013fd692ca42f7d44cec852ba8869d57c9
Kendall Kant <kantkendall@gmail.com>
2022-04-04 09:31:16 -0500

    migrated pages home & main

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,254 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
+  <v-card max-width="2000" class="mx-auto">
+    <v-container>
+      <v-row dense>
+        <v-col cols="12">
+          <v-card color="amber accent-4" dark>
+            <v-card-title class="text-h5"> Public </v-card-title>
 
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-XDV
-        </v-card-title>
-        <v-card-text>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/main"> Sign in</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+            <v-card-subtitle
+              >Data economy services.</v-card-subtitle
+            >
+
+            <v-card-actions>
+              <v-btn text nuxt to="/tag/public"> 21 Dapps, 80 NFTs, 5 alert topics </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+
+        <v-col cols="12">
+          <v-card color="indigo accent-4" dark>
+            <v-card-title class="text-h5"> Shared </v-card-title>
+
+            <v-card-subtitle>Secure messaging.</v-card-subtitle>
+
+            <v-card-actions>
+              <v-btn text nuxt to="/tag/shared"> 16 groups (11 encrypted, 5 open) </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+        <v-col cols="12">
+          <v-card color="blue-grey accent-4" dark>
+            <v-card-title class="text-h5"> Personal </v-card-title>
+
+            <v-card-subtitle>Digital assets and credentials.</v-card-subtitle>
+
+            <v-card-actions>
+              <v-btn text nuxt to="/tag/personal"> 1001 assets, 10 credentials </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
 </template>
 
-<script>
-export default {
-  name: 'IndexPage',
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import { QrcodeCapture } from 'vue-qrcode-reader'
+//@ts-ignore
+import { decode } from 'jsonwebtoken'
+import { CUFEBuilder } from './cufe'
+import { BrowserQRCodeReader } from '@zxing/browser'
+import * as reader from 'promise-file-reader'
+//@ts-ignore
+import BarcodeDetector from 'barcode-detector'
+import 'pdfjs-dist/legacy/build/pdf.worker.entry'
+import { getDocument } from 'pdfjs-dist/legacy/build/pdf.js'
+import { ParkyDB } from 'parkydb'
+
+const payload = {
+  commitHash: 'xg8pyBr3McqYlUgxAqV0t3s6TRcP+B7MHyPTtyVKMJw=',
+  contentHash: {
+    '/': 'baguqeerahiqryfzwbjc2fn7is4k2uupilwtoxabtb6noifnwxznxszuvrg6a',
+  },
+  digest: '0x5ab3124ede7a511fbc7c6302b164d1547aefda8b5909b6bb637c7da025c3ffaf',
+  height: 207,
+  issuer: '0x32A21c1bB6E7C20F547e930b53dAC57f42cd25F6',
+  key: 'YW5jb25wcm90b2NvbC91c2Vycy9kaWQ6ZXRocjpibmJ0OjB4MzJBMjFjMWJCNkU3QzIwRjU0N2U5MzBiNTNkQUM1N2Y0MmNkMjVGNi9iYWd1cWVlcmFoaXFyeWZ6d2JqYzJmbjdpczRrMnV1cGlsd3RveGFidGI2bm9pZm53eHpueHN6dXZyZzZh',
+  lastBlockHash: {
+    '/': 'baguqeeraw4ssfjictj2b47cccep2oioytbjistjke2xuaa35qgdoivtgrwra',
+  },
+  network: 'anconprotocol',
+  parentHash: {
+    '/': 'baguqeerakv6jersryhhanjaihmb2ncdujajxt7gpkfzs7rkur7lo4s5uecwa',
+  },
+  rootKey:
+    'YW5jb25wcm90b2NvbC91c2Vycy9kaWQ6ZXRocjpibmJ0OjB4MzJBMjFjMWJCNkU3QzIwRjU0N2U5MzBiNTNkQUM1N2Y0MmNkMjVGNg==',
+  signature:
+    '0xa628e3e256a187453c55ffbebc189ec4464c6c0d874a278272d224dbbaa4f6f028d3550651c33edf7af0ea8b3f3806d09ba9135034a61dd1a651c67f83cb06a51b',
+  timestamp: 1645384767,
+  content: {
+    blockchainTokenId: '8',
+    blockchainTxHash:
+      '0x977dd680952c4f7da1040d12a0a2c9e68a60f6158edee20b1591f91372030670',
+    creator: '0x32A21c1bB6E7C20F547e930b53dAC57f42cd25F6',
+    currentOrderHash: '',
+    description: 'me and friends',
+    fileExtension: 'jpg',
+    image: 'baguqeerar56ywt7p3qbbf6wiqgja5ybvkblz4wx37vyybthr2jl65d657jha',
+    name: 'Sweden 90s',
+    owner: '0x32A21c1bB6E7C20F547e930b53dAC57f42cd25F6',
+    price: '',
+    sources: [
+      'https://tensta.did.pa/v0/file/baguqeerar56ywt7p3qbbf6wiqgja5ybvkblz4wx37vyybthr2jl65d657jha/',
+    ],
+    tags: 'Diseño grafico',
+    uuid: '854198e4-1c09-4c27-8103-2ae54adfc681',
+  },
+}
+
+@Component({
+  components: {
+    QrcodeCapture,
+  },
+})
+export default class Main extends Vue {
+  bob = new ParkyDB()
+  result = ''
+  selected: any
+  loading = false
+  invoiceQr = ''
+  cedulaQr = ''
+  invoiceQRImage: any
+  cedulaQRImage: any
+  invoice: any
+  cedula: any
+  cedulaId = {}
+  cufe = {}
+  selectedCedula: any
+  feURL = ''
+  cafeModel = { issuer: {}, recipient: {} }
+
+  openDGI() {
+    window.open(this.feURL, '_blank')
+  }
+  async scanInvoice() {
+    const codeReader = new BrowserQRCodeReader()
+    const el = this.$refs.invoice as any
+    const img = BrowserQRCodeReader.prepareImageElement(el.image)
+    img.width = 200
+    img.height = 200
+    const resultImage = await codeReader.decodeFromImageElement(img)
+  }
+
+  async loadInvoice() {
+    if (!this.selected) return
+    this.invoiceQr = this.selected.name
+    this.invoiceQRImage = URL.createObjectURL(this.selected)
+
+    const barcodeDetector = new BarcodeDetector({ formats: ['qr_code'] })
+    const [{ rawValue }] = await barcodeDetector.detect(this.selected)
+
+    this.feURL = rawValue
+    setTimeout(() => {
+      this.decode(rawValue)
+    })
+  }
+
+  async readCAFE() {
+    const b = await reader.readAsArrayBuffer(this.selectedCafe as any)
+    const doc = await getDocument(b as any).promise
+    const page = await doc.getPage(1)
+    const result = await page.getTextContent()
+
+    const items = result.items
+      .filter((i: any) => !i.hasEOL && i.str.length > 1)
+      .map((i: any) => i.str)
+
+    this.cafeModel = {
+      issuer: {
+        name: items[3],
+        ruc: items[5],
+        dv: items[7],
+        address: items[9],
+      },
+      recipient: {
+        type: items[11],
+        name: items[13],
+        ruc: items[15],
+      },
+    }
+  }
+  selectedCafe(selectedCafe: any) {
+    throw new Error('Method not implemented.')
+  }
+  async scanCedula() {
+    if (!this.selectedCedula) return
+    // const datauri = await reader.readAsDataURL(this.selectedCedula)
+    this.cedulaQr = this.selectedCedula.name
+    this.cedulaQRImage = URL.createObjectURL(this.selectedCedula)
+
+    setTimeout(async () => {
+      const codeReader = new BrowserQRCodeReader()
+      const el = this.$refs.cedula as any
+      const img = BrowserQRCodeReader.prepareImageElement(el.image)
+      const resultImage = await codeReader.decodeFromImageElement(img)
+
+      const items = (resultImage as any).text.split('|')
+      this.cedulaId = {
+        id: items[0],
+        name: `${items[1]} ${items[2]}`,
+        dob: items[6],
+        province: items[5],
+        expiration: items[15],
+        issued: items[14],
+      }
+    })
+  }
+  async openMintNFT() {
+    const codeReader = new BrowserQRCodeReader()
+  }
+
+  decode(result: any) {
+    this.result = result
+    this.cufe = ''
+
+    if (result.indexOf('jwt') > 0) {
+      const fe = decode(result.split('jwt')[1].split('=')[1])
+
+      const builder = new CUFEBuilder()
+      const o = builder.fromCUFE(fe.chFE)
+
+      this.cufe = { ...fe, ...o }
+    }
+  }
+
+  async mounted() {
+    const peer =
+      '/dns4/waku.did.pa/tcp/8000/wss/p2p/16Uiu2HAmN96WgFsyepE3tLw67i3j6BdBo3xPF6MQ2hjmbaW5TUoB'
+    try {
+      await this.bob.initialize({
+        wakuconnect: { bootstrap: { peers: [peer] } },
+        // Remember these values come from a CLI or UI, DO NOT hardcode when implementing
+        withWallet: {
+          password: 'zxcvb',
+          seed: 'opera offer craft joke defy team prosper tragic reopen street advice moral',
+        },
+      })
+    } catch (e) {
+    }
+
+    // @ts-ignore
+    await this.bob.wallet.submitPassword(`zxcvb`)
+    const accounts = await this.bob.wallet.getAccounts()
+    const accountB = accounts[0]
+    let id = await this.bob.putBlock(payload)
+
+    if  (typeof id !== 'string') id = id.cid
+    const res = await this.bob.get(id, null)
+    const q = await this.bob.query({
+      cid: id,
+      query: `
+    query{
+       block(cid: "${id}") {
+         network
+         key
+       }
+    }   
+    `,
+    })
+    console.log(q)
+  }
 }
 </script>
